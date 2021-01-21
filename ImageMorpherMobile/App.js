72df8f572d2ba674a4b96657a8f43ad32d50d7cd
Sammy Javed <sjaved@adobe.com>
2021-01-20 21:48:36 -0800

    debugging requests to api, currently sending base64 imgs

--- ImageMorpherMobile/App.js
+++ ImageMorpherMobile/App.js
@@ -1,7 +1,7 @@
 // https://docs.expo.io/versions/latest/sdk/imagepicker/
 
 import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
+import { Button, Image, Text, View, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import Constants from 'expo-constants';
 
@@ -11,6 +11,7 @@ export default function FaceMorpher() {
   // Allows to use state and features w/o making classes.
   const [image1, setImage1] = useState(null);
   const [image2, setImage2] = useState(null);
+  const [morphResponse, setMorphResponse] = useState(null);
 
   // True if first face button is clicked.
   // False if second face button is clicked.
@@ -36,6 +37,7 @@ export default function FaceMorpher() {
       aspect: [4, 3],
       // Quality of compression. (0 - 1.0).
       quality: 1,
+      base64: false,
     });
 
     console.log(result);
@@ -52,9 +54,28 @@ export default function FaceMorpher() {
       }
   }
 
-  const uploadImage = async (image1, image2) => {
-    console.log('uploadImage called')
-  };
+  async function getMorph(image1, image2) {
+    try {
+      let data = new FormData();
+
+      data.append('Image-1', image1);
+      data.append('Image-2', image2);
+      data.append('isSequence', 'True');
+      data.append('stepSize', '20');
+
+      let response = await fetch(
+        'https://sammyjaved.com/morph', {
+          method: 'POST',
+          headers: {
+            'Authorization': 'ImageMorpherV1'
+          },
+          body: data,
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
@@ -62,7 +83,8 @@ export default function FaceMorpher() {
       {image1 && <Image source={{ uri: image1 }} style={{ width: 200, height: 200 }} />}
       <Button title="Pick the second face from camera roll" onPress={pickImage} />
       {image2 && <Image source={{ uri: image2 }} style={{ width: 200, height: 200 }} />}
-      <Button title="MORPH" onPress={uploadImage} />
+      <Button title="MORPH" onPress={() => getMorph(image1, image2)} />
+      {morphResponse && <Text>{morphResponse}</Text> }
     </View>
   );
 }
