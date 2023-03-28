c418c71dba50874f8fc10f8401efe345a7b036f9
Sammy Javed <me@sammyjaved.com>
2023-03-28 13:28:50 +0900

    update dev endpoints to use mymorph

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 // Dev endpoints
-// export const morph_endpoint = 'http://sammyjaved.com:8088/morph'
-// export const morph_upload_endpoint = 'http://sammyjaved.com:8088/morph/upload';
+// export const morph_endpoint = 'http://pyaar.ai:8088/morph'
+// export const morph_upload_endpoint = 'http://pyaar.ai:8088/morph/upload';
 
 // Prod endpoints
 export const morph_endpoint = 'https://pyaar.ai/morph'
