f6d76f4cbda90c6fe51c179d89bff22336ac8ef1
Sammy Javed <me@sammyjaved.com>
2023-04-27 07:11:07 +0900

    Show morph link in async response so user can manually poll Ask for notifications only when morphing

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,11 @@
-// Dev endpoints
+// @deprecated
 // export const morph_endpoint = 'http://pyaar.ai:8088/morph'
 // export const morph_upload_endpoint = 'http://pyaar.ai:8088/morph/upload';
 
+// Dev endpoints
+// export const morph_endpoint = 'https://dev.pyaar.ai/morph';
+// export const morph_upload_endpoint = 'https://dev.pyaar.ai/morph/upload';
+
 // Prod endpoints
 export const morph_endpoint = 'https://pyaar.ai/morph'
-export const morph_upload_endpoint = 'https://pyaar.ai/morph/upload';
+export const morph_upload_endpoint = 'https://pyaar.ai/morph/upload'
